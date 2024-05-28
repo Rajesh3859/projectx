aa48f92f9eda832f8285404e747f566bddce3fc2
Rajesh3859 <rajeshvjm10@gmail.com>
2024-05-28 14:22:33 +0530

    object compare

---
+++ Day3/equality.js
@@ -0,0 +1,18 @@
+let obj1 = { name: "person1", age: 5 };
+let obj2 = { age: 5, name: "person1" };
+
+var a = true;
+
+if (Object.keys(obj1).length == Object.keys(obj2).length) {
+  for (let key in obj1) {
+    if (obj1[key] == obj2[key]) {
+      continue;
+    } else {
+      a = false;
+      break;
+    }
+  }
+} else {
+  a = false;
+}
+console.log(a);
